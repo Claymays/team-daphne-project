56f8bacb2b1080dbebd93908fbb33761c3d2f1bc
erickr19 <erickreyes1902@gmail.com>
2022-11-11 18:31:34 -0600

    Started to add a way to delegate requests based off the query string. Added an instantiatePokemon method that instantiates a pokemon class for the pokemon in the request method.

--- src/controllers/PokedexController.java
+++ src/controllers/PokedexController.java
@@ -20,6 +20,13 @@ public class PokedexController extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //        TODO: how to handle delegation?
+        // from the safari jsp, get the query string from the request made on the page
+        String pokemonQuery = request.getQueryString();
+        // add pokemonQuery to knownPokemon
+        knownPokemon.add(pokemonQuery);
+        // instantiate the pokemon
+        instantiatePokemon(pokemonQuery);
+        //
         request.setAttribute("pokemon", knownPokemon.get(0));
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("./pokedex");
         dispatcher.forward(request, response);
@@ -29,5 +36,46 @@ public class PokedexController extends HttpServlet {
         this.repository = new PokemonRepository();
         repository.pokedexEntry("Bulbasaur", "grass", "a blurb", 001);
         this.knownPokemon = repository.getPokedexEntries();
+    }
+    
+    public void instantiatePokemon(String pokemonName) {
+        // method variables
+        String bio;
+        String type;
+        int entryNumber;
+        // check which pokemon was requested
+        // bulbasaur
+        if (pokemonName == "bulbasaur") {
+            // short bio
+            bio = "A strange seed was planted on its back at birth. The plant sprouts and grows with this Pokemon.";
+            // type
+            type = "grass";
+            // entry number
+            entryNumber = 001;
+            // create pokemon class for bulbasaur
+            Pokemon bulbasaur = new Pokemon(pokemonName, type, bio, entryNumber);
+        }
+        // squirtle
+        if (pokemonName == "squirtle") {
+            // short bio
+            bio = "When it retracts its long neck into its shell, it squirts out water with vigorous force.";
+            // type
+            type = "water";
+            // entry number
+            entryNumber = 003;
+            // create pokemon class for squirtle
+            Pokemon squirtle = new Pokemon(pokemonName, type, bio, entryNumber);
+        }
+        // charmander
+        if (pokemonName == "charmander") {
+            // short bio
+            bio = "A flame burns on the tip of its tail from birth. It is said that a Charmander dies if its flame ever goes out.";
+            // type
+            type = "fire";
+            // entry number
+            entryNumber = 006;
+            // create pokemon class for charmander
+            Pokemon charmander = new Pokemon(pokemonName, type, bio, entryNumber);
+        }
     }
 }
